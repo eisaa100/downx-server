15715a262f5aa3b6159adbb7413474e34f375ac8
eisaa100 <yryad9667@gmail.com>
2025-11-14 01:02:39 +0300

    Update index.js

--- downx-server/index.js
+++ downx-server/index.js
@@ -5,10 +5,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Render يعطيك PORT تلقائياً
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
-  res.json({ status: "Server is running" });
+  res.json({ status: "Server is running ✔️" });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
